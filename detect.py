dcd92fdeb9dfad07dc3b8e557006ee45e5e7bddb
deema1999 <hafezdema1999@gmail.com>
2021-07-14 10:21:12 +0300

    add arduino code and clean the file

--- detect.py
+++ detect.py
@@ -14,20 +14,36 @@ from utils.general import check_img_size, check_requirements, non_max_suppressio
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
+import serial
+import io
+
+
+arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+
+
 def new_video(path):
-        frame = 0
-        cap = cv2.VideoCapture(path)
-        nframes = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        return nframes
+    frame = 0
+    cap = cv2.VideoCapture(path)
+    nframes = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    return nframes
+
+
+speed = 0
+buzzer = 0
+
 
 def detect(save_img=False):
+    speed = 0
+    buzzer = 0
     source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://'))
 
     # Directories
-    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    save_dir = Path(increment_path(Path(opt.project) / opt.name,
+                    exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True,
+                                                          exist_ok=True)  # make dir
 
     # Initialize
     set_logging()
@@ -45,7 +61,8 @@ def detect(save_img=False):
     classify = False
     if classify:
         modelc = load_classifier(name='resnet101', n=2)  # initialize
-        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+        modelc.load_state_dict(torch.load(
+            'weights/resnet101.pt', map_location=device)['model']).to(device).eval()
 
     # Set Dataloader
     vid_path, vid_writer = None, None
@@ -63,10 +80,11 @@ def detect(save_img=False):
 
     # Run inference
     if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(
+            next(model.parameters())))  # run once
     t0 = time.time()
-    
-    for path, img, im0s, vid_cap in dataset:  
+    print("path", "path")
+    for path, img, im0s, vid_cap in dataset:
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -78,7 +96,8 @@ def detect(save_img=False):
         pred = model(img, augment=opt.augment)[0]
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(
+            pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
         t2 = time_synchronized()
 
         # Apply Classifier
@@ -87,8 +106,10 @@ def detect(save_img=False):
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
+            File = io.open("data_____"+str(i)+".csv", "w", encoding="utf-8")
             if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(
+                ), dataset.count
             else:
                 p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
                 nFrames = new_video(p)
@@ -96,56 +117,103 @@ def detect(save_img=False):
 
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # img.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            txt_path = str(save_dir / 'labels' / p.stem) + \
+                ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
             s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            # normalization gain whwh
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
             if len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                det[:, :4] = scale_coords(
+                    img.shape[2:], det[:, :4], im0.shape).round()
 
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    # add to string
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
 
-                # Write results
-                # for *xyxy, conf, cls in reversed(det):
-                #     if save_txt:  # Write to file
-                #         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                #         line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
-                #         with open(txt_path + '.txt', 'a') as f:
-                #             f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                boxSize = 0
+                maxBoxSize = 0
 
-                #     if save_img or view_img:  # Add bbox to image
-                #         label = f'{names[int(cls)]} {conf:.2f}'
-                #         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-                f.open("resFile" + '.txt', 'a') as resFile
+                #arduino.write(bytes('0', 'utf-8'))
                 for *xyxy, conf, cls in reversed(det):
-                    if int(cls) == 0 :
-                        startPoint= (int(xyxy[0]),int(xyxy[1]))
-                        endPoint= (int(xyxy[2]),int(xyxy[3])) 
-                        ar = (int(xyxy[2])-int(xyxy[0]))*(int(xyxy[1])-int(xyxy[3]))
-                        labelNew = f'{names[int(cls)]} {conf:.2f}'
-                        print("<----->", startPoint , endPoint ,labelNew ,"--->", int(cls) , "......." ,abs(ar))
 
+                    if int(cls) == 0:
+                        startPoint = (int(xyxy[0]), int(xyxy[1]))
+                        endPoint = (int(xyxy[2]), int(xyxy[3]))
+                        boxSize = (
+                            abs(int(xyxy[2])-int(xyxy[0]))*abs(int(xyxy[1])-int(xyxy[3])))
+                        label = f'{maxBoxSize} {boxSize} {conf:.2f} {speed} {buzzer}'
+                        print("<----->", startPoint, endPoint, label,
+                              "--->", int(cls), ".......", abs(boxSize))
+
+                        if (int(xyxy[0])) >= 323:
+                            y = (int(xyxy[0]) * 1.0441) - 120.235
+                            #y = (int(xyxy[0]) * 3.1797752809 ) - 443.202247191
+
+                            if int(xyxy[3]) >= y:
+
+                                if boxSize > maxBoxSize:
+                                    maxBoxSize = boxSize
+
+                                if save_img or view_img:  # Add bbox to image
+                                    # label = f'{names[int(cls)]} {boxSize} {conf:.2f}'
+                                    plot_one_box(
+                                        xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                            else:
+                                if save_img or view_img:  # Add bbox to image
+                                    # label = f'{names[int(cls)]} {boxSize} {conf:.2f}'
+                                    plot_one_box(
+                                        xyxy, im0, label=label, color=colors[int(1)], line_thickness=3)
+
+                        else:
+                            y2 = -(int(xyxy[2]) * 0.55196) + 395.48
+                            #y2 = -(int(xyxy[0]) * 0.28896103896 ) + 576.590909091
+                            print(y2)
+                            if int(xyxy[3]) >= y2:
+
+                                if boxSize > maxBoxSize:
+                                    maxBoxSize = boxSize
+
+                                if save_img or view_img:  # Add bbox to image
+
+                                    plot_one_box(
+                                        xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                            else:
+                                if save_img or view_img:  # Add bbox to image
+                                    # label = f'{names[int(cls)]} {boxSize} {conf:.2f}'
+                                    plot_one_box(
+                                        xyxy, im0, label=label, color=colors[int(1)], line_thickness=3)
 
                         if save_txt:  # Write to file
-                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                            line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(
+                                1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                            # label format
+                            line = (
+                                cls, *xywh, conf) if opt.save_conf else (cls, *xywh)
                             with open(txt_path + '.txt', 'a') as f:
-                                f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                                f.write(('%g ' * len(line)).rstrip() %
+                                        line + '\n')
 
-                        if save_img or view_img:  # Add bbox to image
-                            label = f'{names[int(cls)]} {conf:.2f}'
-                            plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                if(maxBoxSize > 4000):
+                    arduino.write(bytes('300', 'utf-8'))
+                    buzzer = 1
+
+                else:
+                    speed = (maxBoxSize-300) * (255-110)/(4000 - 300) + 1
+                    speed = max(speed, 0)
+                    speed = 255 - speed
+                    arduino.write(bytes(str(speed), 'utf-8'))
+                    buzzer = 0
 
             # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)')
-            
+            print(f'{s}Done. {0} ({t2 - t1:.3f}s)')
+
             # Stream results
             if view_img:
                 cv2.imshow(str(p), im0)
-                cv2.waitKey(1) # 1 millisecond
+                cv2.waitKey(1)  # 1 millisecond
 
             # Save results (image with detections)
             if save_img:
@@ -161,7 +229,8 @@ def detect(save_img=False):
                         fps = vid_cap.get(cv2.CAP_PROP_FPS)
                         w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                         h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+                        vid_writer = cv2.VideoWriter(
+                            save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
                     vid_writer.write(im0)
 
     if save_txt or save_img:
@@ -171,24 +240,42 @@ def detect(save_img=False):
     print(f'Done. ({time.time() - t0:.3f}s)')
     print("FPS: ", nFrames/(time.time() - t0))
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--weights', nargs='+', type=str,
+                        default='yolov5s.pt', help='model.pt path(s)')
+    # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str,
+                        default='data/images', help='source')
+    parser.add_argument('--img-size', type=int, default=640,
+                        help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float,
+                        default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float,
+                        default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='',
+                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true',
+                        help='display results')
+    parser.add_argument('--save-txt', action='store_true',
+                        help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true',
+                        help='save confidences in --save-txt labels')
+    parser.add_argument('--classes', nargs='+', type=int,
+                        help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true',
+                        help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true',
+                        help='augmented inference')
+    parser.add_argument('--update', action='store_true',
+                        help='update all models')
+    parser.add_argument('--project', default='runs/detect',
+                        help='save results to project/name')
+    parser.add_argument('--name', default='exp',
+                        help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true',
+                        help='existing project/name ok, do not increment')
     opt = parser.parse_args()
     print(opt)
     check_requirements()
@@ -200,3 +287,5 @@ if __name__ == '__main__':
                 strip_optimizer(opt.weights)
         else:
             detect()
+
+    # File.close()
